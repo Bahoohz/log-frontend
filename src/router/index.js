50e10013a7452a6b5227ffaf37f3da97e102e15c
Worawit Werapan <werapan@go.buu.ac.th>
2020-02-11 20:24:35 +0700

    Initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,40 @@ const routes = [
     component: Home
   },
   {
+    path: '/form',
+    name: 'form',
+    component: () => import('../views/Form.vue')
+  },
+  {
+    path: '/showview',
+    name: 'showview',
+    component: () => import('../views/ShowView.vue')
+  },
+  {
+    path: '/table',
+    name: 'table',
+    component: () => import('../views/Table.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/counter',
+    name: 'counter',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Counter.vue')
   }
 ]
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <b-img :src="require('../assets/myimage.jpg')"></b-img>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
